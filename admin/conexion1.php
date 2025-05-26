c23aa38589fc86dae8301166f6e018ae627eb752
CarlosDavid <carlos.david053@pascualbravo.edu.co>
2025-05-26 18:14:32 -0500

    first commit

--- admin/conexion1.php
+++ admin/conexion1.php
@@ -1,9 +1,9 @@
 <?php
 
-$servidor="seguridadenredes.c6bkqwk8eqtu.us-east-1.rds.amazonaws.com"; //Colocamos el nombre del servidor en el cual esta la base de datos
+$servidor="localhost"; //Colocamos el nombre del servidor en el cual esta la base de datos
 $nombreBd="biblioteca";//Colocamos el nombre de nuestra base de datos
-$usuario="admin"; //Colocamos el usuario del sevidor al cual vamos a ingresar
-$password="VE9RzUIFtZ4A08GYHGQ8"; //Ingresamos contraseña si el servidor la tiene sino pues se deja vacía
+$usuario="root"; //Colocamos el usuario del sevidor al cual vamos a ingresar
+$password=""; //Ingresamos contraseña si el servidor la tiene sino pues se deja vacía
 
 
 //Creamos la variable conexión en la que se almacenará la información de todo el ingreso a la base de datos 
